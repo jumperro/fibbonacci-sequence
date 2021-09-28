bde8392d4d6e71a77005792105fc48f4dd6c5fe8
jumperro <45234153+jumperro@users.noreply.github.com>
2021-09-28 11:34:13 +0200

    description added

--- fibbonacci.java
+++ fibbonacci.java
@@ -5,11 +5,13 @@ public class fibbonacci {
         int zm1, zm2, x;
         zm1 = 0;
         zm2 = 1;
-        for(int i=1;i<15;i++){
+         System.out.println("FIBBONACCI SEQUENCE\n\nFIRST FIFTEEN NUMBERS :\n");
+        System.out.print("| ");
+        for(int i=1;i<16;i++){
             x=zm1+zm2;
             zm2=zm1;
             zm1=x;
-            System.out.print(x+" ,");
+            System.out.print(x+" | ");
         }
     }
 }
