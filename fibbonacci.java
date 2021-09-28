22b7ccbea2c513883a949d9d9aacf5d17cdcbaac
jumperro <45234153+jumperro@users.noreply.github.com>
2021-09-28 11:36:04 +0200

    name of the project changed

--- fibbonacci.java
+++ fibbonacci.java
@@ -1,11 +1,11 @@
 package podstawy;
 
-public class fibbonacci {
+public class fibonacci {
     public static void main(String[] args) {
         int zm1, zm2, x;
         zm1 = 0;
         zm2 = 1;
-         System.out.println("FIBBONACCI SEQUENCE\n\nFIRST FIFTEEN NUMBERS :\n");
+         System.out.println("FIBONACCI SEQUENCE\n\nFIRST FIFTEEN NUMBERS :\n");
         System.out.print("| ");
         for(int i=1;i<16;i++){
             x=zm1+zm2;
